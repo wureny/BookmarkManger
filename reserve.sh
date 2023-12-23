5d52143ae77aa538df3b110219bba2fee38b0e71
wureny <wurenyu19@gmail.com>
2023-12-23 15:21:27 +0800

    add sh

---
+++ reserve.sh
@@ -0,0 +1,6 @@
+## Change the params below.
+## Then you can get your own on-chain bookmark.
+register-evm-agent reserve -k <private_key> -g <gas price> -n <network> -i <identity_path> --evm <evm_canister_principal> --canister-id <reserve_canister_principal>
+
+
+
